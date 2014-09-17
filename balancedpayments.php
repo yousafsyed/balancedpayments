01ab450c9a76b4c0de9303590edf7df928958847
yousafsyed <mmesunny@gmail.com>
2014-09-17 23:48:16 +0100

    updates

--- balancedpayments.php
+++ balancedpayments.php
@@ -2,112 +2,75 @@
 require ("helper/BalancedHTTP.php");
 
 class Balancedpayments {
-	private static $market_place;
-	private static $apikey;
-	private static $statement_appear_as;
-	private static $payment_description;
-	public static $cards = 'https://api.balancedpayments.com/cards';
-	public static $users = 'https://api.balancedpayments.com/customers';
+private static $market_place;
+private static $apikey;
+private static $statement_appear_as;
+private static $payment_description;
+public static $cards = 'https://api.balancedpayments.com/cards';
+public static $users = 'https://api.balancedpayments.com/customers';
 
-	public static function config($config) {
-		self::$apikey              = $config['apikey'];
-		self::$statement_appear_as = $config['statement_appear_as'];
-		self::$payment_description = $config['payment_description'];
-	}
+public static function config($config) {
+self::$apikey              = $config['apikey'];
+self::$statement_appear_as = $config['statement_appear_as'];
+self::$payment_description = $config['payment_description'];
+}
 
 	public static function create_user($userdata) {
-		if (is_array($userdata)) {
-			if (key_exists('name', $userdata)) {
 
-				$response  = BalancedHTTP::post(self::$users, null, $userdata, self::$apikey, '');
-				$user_data = json_decode($response->raw_body, true);
-				if (key_exists('customers', $user_data)) {
-					return $user_data['customers'][0]['id'];
-				} else {
-					echo $user_data['errors'][0]['description'];
-					return false;
-				}
-			} else {
-				return false;
-			}
-		} else {
-			echo "ERROR: Parameter passed to Balancedpayments::create_user must be an array";
-		}
-	}
+	$response  = BalancedHTTP::post(self::$users, null, $userdata, self::$apikey, '');
+	$user_data = json_decode($response->raw_body, true);
+
+	return $user_data;
+}
 
 	public static function tokenize_card($cards_parameter) {
-		if (key_exists('card_number', $cards_parameter) && key_exists('expiration_month', $cards_parameter) && key_exists('expiration_year', $cards_parameter) && key_exists('security_code', $cards_parameter)) {
 
-			$response  = BalancedHTTP::post(self::$cards, null, $cards_parameter, self::$apikey, '');
-			$card_data = json_decode($response->raw_body, true);
+	$response  = BalancedHTTP::post(self::$cards, null, $cards_parameter, self::$apikey, '');
+	$card_data = json_decode($response->raw_body, true);
 
-			if (key_exists('uri', $card_data)) {
-				return $card_data['uri'];
-			} else {
-				return array('error' => $card_data['description']);
-			}
-		} else {
-			return false;
-		}
-	}
+	return $card_data;
+
+}
 
 	public static function add_card($customer_id, $card_id) {
-		$user_parameter = array('customer' => '/customers/'.$customer_id);
+	$user_parameter = array('customer' => '/customers/' . $customer_id);
 
-		$response  = BalancedHTTP::put(self::$cards.'/'.$card_id, null, $user_parameter, self::$apikey, '');
-		$user_data = json_decode($response->raw_body, true);
+	$response  = BalancedHTTP::put(self::$cards . '/' . $card_id, null, $user_parameter, self::$apikey, '');
+	$user_data = json_decode($response->raw_body, true);
+	return $user_data;
 
-		if (!key_exists('errors', $user_data)) {
-			return $user_data['cards'][0]['links']['customer'];
-		} else {
-
-			return array('error' => $user_data['errors'][0]['description']);
-		}
-	}
+}
 
 	public static function charge_card($card_id, $amount) {
-		$biling_parameters = array('appears_on_statement_as' => self::$statement_appear_as, 'amount' => $amount*100, 'description' => self::$payment_description);
-		$response          = BalancedHTTP::post(self::$cards.'/'.$card_id.'/debits', null, $biling_parameters, self::$apikey, '');
-		$debited           = json_decode($response->raw_body, true);
+	$biling_parameters = array('appears_on_statement_as' => self::$statement_appear_as, 'amount' => $amount * 100, 'description' => self::$payment_description);
+	$response          = BalancedHTTP::post(self::$cards . '/' . $card_id . '/debits', null, $biling_parameters, self::$apikey, '');
+	$debited           = json_decode($response->raw_body, true);
 
-		if (!key_exists('errors', $debited)) {
-			return true;
-		} else {
-
-			return array('error' => $debited['errors'][0]['description']);
-		}
-	}
+	return $debited;
+}
 
 	public static function delete_user($customer_id) {
-		$response = BalancedHTTP::post(self::$users.'/'.$customer_id, null, null, self::$apikey, '');
-		$deleted  = json_decode($response->raw_body, true);
-		if (key_exists('status', $deleted)) {
-			if ($debited['status'] == 'succeeded') {
-				return true;
-			} else {
-				return array('error' => $deleted['description']);
-			}
-		} else {
-			return false;
-		}
-	}
+	$response = BalancedHTTP::post(self::$users . '/' . $customer_id, null, null, self::$apikey, '');
+	$deleted  = json_decode($response->raw_body, true);
+	return $deleted;
+}
 
 	public static function delete_card($card_id) {
-		$response = BalancedHTTP::delete(self::$cards.'/'.$card_id, null, null, self::$apikey, '');
-		$deleted  = json_decode($response->raw_body, true);
+	$response = BalancedHTTP::delete(self::$cards . '/' . $card_id, null, null, self::$apikey, '');
+	$deleted  = json_decode($response->raw_body, true);
 
-		return true;
-	}
+	return $deleted;
+}
 
 	public static function list_cards() {
-		$response = BalancedHTTP::get(self::$cards, null, null, self::$apikey, '');
-		$cards    = json_decode($response->raw_body, true);
-		return $cards['cards'];
-	}
+	$response = BalancedHTTP::get(self::$cards, null, null, self::$apikey, '');
+	$cards    = json_decode($response->raw_body, true);
+	return $cards;
+}
 
 	public static function get_card($card_id) {
-		$response = BalancedHTTP::get(self::$cards.'/'.$card_id, null, null, self::$apikey, '');
-		$cards    = json_decode($response->raw_body, true);
-		return $cards['cards'][0];
-	}
+	$response = BalancedHTTP::get(self::$cards . '/' . $card_id, null, null, self::$apikey, '');
+	$cards    = json_decode($response->raw_body, true);
+	return $cards;
+}
 }
--- example.php
+++ example.php
@@ -3,43 +3,48 @@
 require ("balancedpayments.php");
 
 $config = array(
-	"apikey"              => "ak-test-1iyjUQ7UcMu6N20Aub3GayZsSARhLygoR", // set your api key
-	"payment_description" => " Charging the card", // set the payment description
-	"statement_appear_as" => "Example Company"// set the statement
+"apikey"              => "ak-test-1iyjUQ7UcMu6N20Aub3GayZsSARhLygoR", // set your api key
+"payment_description" => " Charging the card", // set the payment description
+"statement_appear_as" => "Example Company"// set the statement
 
 );
 
-Balancedpayments::config($config);
+	Balancedpayments::config($config);
 
-/**
- * Create customer example
- * */
-$userdata = array(
-	"name"          => "yousaf",
-	"email"         => "mmesunny@gmail.com",
+	/**
+	 * Create customer example
+	 * */
+	$userdata = array(
+	"name"          => "Sunny Ehsan",
+	"email"         => "me@yousafsyed.com",
 	"adddres"       => "NY street 1",
 	"meta[user_id]" => "123"
 );
-$customer_id = Balancedpayments::create_user($userdata);
+	$customer_array = Balancedpayments::create_user($userdata);
+	$customer_id    = $customer_array['customers'][0]['id'];
+	print_r($customer_id);
 
-/**
- * Tokenize Card
- * */
-$card_params = array(
-	"card_number"      => "4111111111111111",
+	/**
+	 * Tokenize Card
+	 * */
+	$card_params = array(
+	"number"           => "4111111111111111",
 	"expiration_month" => "08",
 	"expiration_year"  => "2017",
 	"security_code"    => "123"
 );
-$card_id = Balancedpayments::tokenize_card($card_params);
+	$card_info_array = Balancedpayments::tokenize_card($card_params);
+	$card_id         = $card_info_array['cards'][0]['id'];
+	//print_r($card_info_array);
+	/**
+	 * Attach card to the customer
+	 * */
+	$addcard = Balancedpayments::add_card($customer_id, $card_id);
+	//print_r($addcard);
 
-/**
- * Attach card to the customer
- * */
-Balancedpayments::add_card($customer_id, $card_id);
-
-/**
- * Charge The Card
- * */
-$amount = "100";// 100 USD
-Balancedpayments::charge_card($card_id, $amount);
+	/**
+	 * Charge The Card
+	 * */
+	$amount   = "100";// 100 USD
+	$response = Balancedpayments::charge_card($card_id, $amount);
+	//print_r($response)
